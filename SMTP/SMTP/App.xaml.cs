14a97ed832697cac4c9ff8e6437ca92a5ffd3c9d
toghrulaziz <togrul1609@gmail.com>
2023-08-28 00:23:02 +0400

    SMTP

---
+++ SMTP/SMTP/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using SMTP.ViewModels;
+using SMTP.Views;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SMTP
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public new MainView? MainWindow { get; set; }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            MainWindow = new MainView
+            {
+                DataContext = new MainViewModel()
+            };
+            MainWindow.Show();
+        }
+    }
+}
---
+++ SMTP/SMTP/ViewModels/MainViewModel.cs
@@ -0,0 +1,82 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SMTP.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+
+
+		private string? toAddress;
+
+		public string? ToAddress
+		{
+			get { return toAddress; }
+			set { Set(ref toAddress, value); }
+		}
+
+
+
+		private string? subject;
+
+		public string? Subject
+		{
+			get { return subject; }
+			set { Set(ref subject, value); }
+		}
+
+
+
+		private string? message;
+
+		public string? Message
+		{
+			get { return message; }
+			set { Set(ref message, value); }
+		}
+
+
+        public RelayCommand SendCommand
+        {
+            get => new RelayCommand(() =>
+            {
+				try
+				{
+					MailAddress fromAddress = new("togrul1609@gmail.com", "Togrul Azizli");
+					MailAddress toAddress = new($"{ToAddress}");
+
+					MailMessage message = new(fromAddress, toAddress);
+
+					message.Subject = Subject;
+					message.IsBodyHtml = true;
+					message.Body = $"<html><body><h1>{Message}</h1></body></html>";
+
+					SmtpClient smtpClient = new("smtp.gmail.com", 587);
+
+					smtpClient.Credentials = new NetworkCredential("togrul1609@gmail.com", "bhguvayglxtrxpgx");
+					smtpClient.EnableSsl = true;
+
+					smtpClient.Send(message);
+
+					MessageBox.Show("Message Sent!");
+
+				}
+				catch (Exception ex)
+				{
+					MessageBox.Show(ex.Message);
+				}	
+
+            });
+        }
+
+
+    }
+}
